ca553b815bfa1b07c68dd08751e55bfa97567fa8
Faizan kaishar <115065680+faiz1487@users.noreply.github.com>
2026-01-17 11:27:10 +0530

    Change S3 bucket name in provider.tf

--- Jenkins-Sonarqube-VM-Terraform/provider.tf
+++ Jenkins-Sonarqube-VM-Terraform/provider.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "varshita-s3-demo-buckett"
+    bucket = "faiz12-s3-demo-buckett"
     key = "terraform.tfstate"
     region = "us-east-1"
     use_lockfile = true
